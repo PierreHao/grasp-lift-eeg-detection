649e9afe51afef6d02933c14d70b1372343915de
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-12-16 12:54:58 -0500

    changes for running the gaussian svm

--- gaussian_vlad_pipeline.py
+++ gaussian_vlad_pipeline.py
@@ -13,11 +13,12 @@ from sklearn.preprocessing import StandardScaler
 from vlad import Vlad
 
 import sys
+import csv
 
 DATA_DIR = "data/processed/"
 N_COMPONENT = sys.argv[1]
 
-subjects = range(1, 12)
+subjects = range(1, 13)
 
 X =  np.concatenate([np.load("{0}/{1}/subj{2}_train_data.npy".format(DATA_DIR, N_COMPONENT, subject)) for subject in subjects])
 y =  np.concatenate([np.load("{0}/{1}/subj{2}_train_labels.npy".format(DATA_DIR, N_COMPONENT, subject)) for subject in subjects])
@@ -31,7 +32,7 @@ y_test = y_test[:,2]
 print(X.shape, y.shape)
 print(X_test.shape, y_test.shape)
 
-clf = svm.SVC(kernel='linear',C=1)
+clf = svm.SVC(kernel='rbf',C=1)
 myVlad = Vlad()
 pca = PCA(n_components=0.9)
 scaler = StandardScaler()
@@ -43,7 +44,7 @@ num_clusters = [2**9, 2**10, 2**11, 2**12]
 cGrid=[2**-3, 2**-2, 2**-1, 2**0, 2**1, 2**2, 2**3]
 gammaGrid=[2**-3, 2**-2, 2**-1, 2**0, 2**1, 2**2, 2**3]
 
-estimator = GridSearchCV(vlad_pipeline, dict(myown__num_clusters=num_clusters,svm__C=cGrid), n_jobs =12 )
+estimator = GridSearchCV(vlad_pipeline, dict(myown__num_clusters=num_clusters,svm__C=cGrid, svm__gamma=gammaGrid), n_jobs =12 )
 estimator.fit(X,y)
 estimator.predict(X_test)
 
@@ -57,25 +58,24 @@ predictions_binary=label_binarize(predictions,classes=[1,2,3,4,5,6])
 
 aucTotal = 0
 for i in range(0,6):
-	singleAuc=roc_auc_score(y_binary[:,i],predictions_binary[:,i])
-	aucTotal+=singleAuc
-	print("for label",i,"auc=",singleAuc)
+    singleAuc=roc_auc_score(y_binary[:,i],predictions_binary[:,i])
+    aucTotal+=singleAuc
+    print("for label",i,"auc=",singleAuc)
 
 print("ACU score ", aucTotal/6)
 
-fileName = "AUC_"+N_COMPONENT+"components.csv"
-with open(fileName, "a") as myfile:
-    myfile.write("svm__C, myown__num_clusters,mean_validation_score,cv_validation_scores\n")
+fileName = "AUC_"+str(N_COMPONENT)+"components.csv"
+with open(fileName, "w") as myfile:
+    writer = csv.writer(myfile, delimiter = ",")
+    paramKeys = list(estimator.grid_scores_[0].parameters.keys())
+
+    writer.writerow(['mean']+ paramKeys)
+    
     for i in estimator.grid_scores_:
-	#fileLine= i, "splitValues: ",estimator.grid_scores_[0].cv_validation_scores
-	#myfile.write(fileLine)
-	#myfile.write("/n")
-	#print fileLine
-	myfile.write(str(i.parameters.get("svm__C")))
-	myfile.write(", ")
-	myfile.write(str(i.parameters.get("myown__num_clusters")))
-        myfile.write(", ")
-        myfile.write(str(i.mean_validation_score))
-        myfile.write(", ")
-        myfile.write(str(i.cv_validation_scores)[1:-1])
-	myfile.write("\n")
+        output = list()
+        output.append(i.mean_validation_score)
+
+        for k in paramKeys:
+            output.append(i.parameters.get(k))
+
+        writer.writerow(output)
